427a03e2e4cecbfbc62ad24870d105915f2e7ef9
katieyang <13hy22@queensu.ca>
2026-09-12 10:59:45 -0700

    completing debugging exercises

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -10,7 +10,7 @@ def decrement_smallest_value(nested_array)
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_valu
+        smallest_value = current_value
       end
     end
   end
@@ -21,14 +21,14 @@ end
 # Don't look at this method prior to running the test
 # Run rspec, let the test fail, and go through the stack trace again
 def increment_greatest_value(nested_array)
-  greatest_value = nested_array.flatten.min
-  nested_array.each do |array|
-    array.each do |current_value|
-      if greatest_value < nil
-        greatest_value = current_value
-      end
-    end
-  end
+  greatest_value = nested_array.flatten.max
+  # nested_array.each do |array|
+  #   array.each do |current_value|
+  #     if greatest_value < nil
+  #       greatest_value = current_value
+  #     end
+  #   end
+  # end
   greatest_value += 1
 end
 
@@ -37,7 +37,7 @@ end
 
 def isogram?(string)
   original_length = string.length
-  string_array = string.downcase.split
+  string_array = string.downcase.split("")
   unique_length = string_array.uniq.length
   original_length == unique_length
 end
@@ -51,6 +51,8 @@ end
 
 def yell_greeting(string)
   name = string
-  name = name.downcase
+  name = name.upcase
   greeting = "WASSAP, #{name}!"
 end
+
+# increment_greatest_value([[3, 4], [1, 2]])
